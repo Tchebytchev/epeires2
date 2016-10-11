bd9d021741034abb97a508012995137ae86fa79e
Tchebytchev <guillaume.hoeltzel@orange.fr>
2016-10-11 18:09:14 +0200

    Planning button

---
+++ module/Agenda/config/module.config.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ * This file is part of Epeires².
+ * Epeires² is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Epeires² is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Epeires². If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+return array(
+    'router' => array(
+        'routes' => array(
+            'agenda' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/agenda[/:controller[/:action]]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*'
+                    ),
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Agenda\Controller',
+                        'controller' => 'Index',
+                        'action' => 'index'
+                    )
+                )
+            )
+        )
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory'
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator'
+        )
+    ),
+    'translator' => array(
+        'locale' => 'fr_FR',
+        'translation_file_patterns' => array(
+            array(
+                'type' => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern' => '%s.mo'
+            )
+        )
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            'agenda_entities' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/Agenda/Entity'
+                )
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Agenda\Entity' => 'agenda_entities'
+                )
+            )
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Agenda\Controller\Index' => 'Agenda\Controller\IndexController',
+            'Agenda\Controller\Report' => 'Agenda\Controller\ReportController'
+        )
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+            'agenda/layout' => __DIR__ . '/../view/layout/agendalayout.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml'
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+            __DIR__ . '/../view/agenda'
+        )
+    ),
+    /**
+     * Automatically use module assets
+     */
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../public'
+            )
+        )
+    ),
+    
+    'permissions' => array(
+        'Agenda' => array(
+            'agenda.read' => array(
+                'name' => 'Accès',
+                'description' => ''
+            )
+        )
+    ),
+    'zfc_rbac' => array(
+        'guards' => array(
+            'ZfcRbac\Guard\RoutePermissionsGuard' => array(
+                'agenda' => array(
+                    'agenda.read'
+                )
+            ),
+        )
+    )
+);
